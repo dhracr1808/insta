db7777fd84a8141e3ba686d317aa5a1ba1951678
diego <diegohuarcayarojas@gmail.com>
2022-05-06 10:29:35 -0500

    make it better

--- dist/config/config.js
+++ dist/config/config.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.PORT = void 0;
+exports.PORT = exports.DBUSERNAME = exports.DBPASSWORD = exports.DBNAME = void 0;
 
 var _dotenv = _interopRequireDefault(require("dotenv"));
 
@@ -13,3 +13,9 @@ _dotenv["default"].config();
 
 var PORT = process.env.PORT || 4001;
 exports.PORT = PORT;
+var DBNAME = process.env.DBNAME;
+exports.DBNAME = DBNAME;
+var DBUSERNAME = process.env.DBUSERNAME;
+exports.DBUSERNAME = DBUSERNAME;
+var DBPASSWORD = process.env.DBPASSWORD;
+exports.DBPASSWORD = DBPASSWORD;
--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,19 @@
 "use strict";
 
-var _express = _interopRequireDefault(require("express"));
+var _app = _interopRequireDefault(require("./app"));
 
 var _config = require("./config/config");
 
+var _User = require("./models/User");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var app = (0, _express["default"])();
-app.get("/", function (req, res) {
-  res.send("hola mundo");
-});
-app.listen(_config.PORT, function () {
-  console.log("server on port", _config.PORT);
+_User.User.sync({
+  force: false
+}).then(function () {
+  _app["default"].listen(_config.PORT, function () {
+    console.log("db connected and server on port", _config.PORT);
+  });
+})["catch"](function (err) {
+  console.log("upps  err");
 });
